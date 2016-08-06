eac6565936aa8fb53cc538cf09217b3e3cfea982
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-06 22:21:54 +0300

    integrate socket server

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 //Require node modules
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 //create a home route
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('index.html');
 });
 
+//using socket.io
+io.on('connection', function(socket){
+  console.log('A user is connected');
+});
 //start the server
 http.listen(3000, function(){
   console.log('listening on port:3000');
