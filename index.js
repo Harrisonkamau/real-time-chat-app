f853e4d9d688624bf87f8932e726c5aa70de933c
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-06 21:49:33 +0300

    add comments

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+//Require node modules
 var app = require('express')();
 var http = require('http').Server(app);
 
+//create a home route
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//start the server
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on port:3000');
 });
